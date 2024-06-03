588ab3531ac632653b67881cd40f7d58be96bbe2
Arelys Alvarez <aalva456@yahoo.com>
2024-06-03 17:37:21 -0400

    adds workflows for front end and back end deployment for ci/cd

--- client/src/api/model_predictions_api.tsx
+++ client/src/api/model_predictions_api.tsx
@@ -2,9 +2,10 @@ import axios from 'axios'
 import {TrackAudioFeatures} from "../utils/trackTypes";
 import {type} from "os";
 
+const PREDICTIONS_ENDPOINT = 'http://localhost:5000/predict'
 
 export const predictTrackMood = (trackFeatures: TrackAudioFeatures[]) => {
-    return axios.post('http://localhost:5000/predict', {trackFeatures})
+    return axios.post(PREDICTIONS_ENDPOINT, {trackFeatures})
         .then(response => {
             const prediction = response.data.prediction;
             // console.log('Prediction:', prediction);
