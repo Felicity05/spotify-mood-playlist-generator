8b1773253582bb348bc6c1f6cfe87381149d987c
Arelys Alvarez <aalva456@yahoo.com>
2024-06-07 17:46:57 -0400

    [frontend] fixes typos on Home page (test github actions deployment)

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -18,8 +18,8 @@ export const Home = () => {
                     flexDirection: "column",
                     color: "white",
                 }}>
-                    <h1>Welcome Moodify</h1>
-                    <h3>A Mood Generator Playlist for Spotify</h3>
+                    <h1>Welcome to Moodify</h1>
+                    <h3>Your Mood Generator Playlist for Spotify</h3>
                     <div style={{display: "flex", alignItems: "center", flexDirection: "column"}}>
                         <img src={music_mood} alt='music mood' width={'30%'}/>
                         <h2>Please log in to get started</h2>
